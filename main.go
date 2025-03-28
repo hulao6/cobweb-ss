057ab03335b41ad4527f95d288bd543f7d9ef671
shynome <shynome@gmail.com>
2025-03-28 17:13:05 +0800

    备份时忽略日志数据库

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
 	_ "github.com/shynome/cobweb/v3/migrations"
 	"github.com/shynome/err0/try"
 )
@@ -16,5 +17,9 @@ func main() {
 	app.RootCmd.PersistentFlags().StringVar(&wspath, "wspath", "/ray", "v2ray websocket path")
 	app.RootCmd.PersistentFlags().StringVar(&wsfilepath, "wsfilepath", "./ws.socket", "v2ray unix socket filepath")
 	app.OnServe().BindFunc(initV2ray)
+	app.OnBackupCreate().BindFunc(func(e *core.BackupEvent) error {
+		e.Exclude = append(e.Exclude, "auxiliary.db", "auxiliary.db-shm", "auxiliary.db-wal")
+		return e.Next()
+	})
 	try.To(app.Start())
 }
